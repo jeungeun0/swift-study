22c0e353241eda78c5ca9d361a3bee782400d0c2
jeung-dev <aisdugo@gmail.com>
2022-05-12 12:40:35 +0900

    ### 위치권한 받아오는 코드 추가 1. 위치권한을 묻는 Info.plist 항목 추가 2. 위치권한을 받아오는 작업 코드 추가 3. 위치를 받아서 로그를 찍도록 코드 추가

--- StudyMapView/ViewController.swift
+++ StudyMapView/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
+    
+    let mapView: MKMapView = MKMapView()
+    
+    lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mapviewSetting()
     }
 
-
+    func mapviewSetting() {
+        getLocationUsagePermission()
+    }
+    
+    func getLocationUsagePermission() {
+        self.locationManager.requestWhenInUseAuthorization()
+    }
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted, .notDetermined:
+            Logger.d("GPS권한 설정되지 않음.")
+            getLocationUsagePermission()
+        case .denied:
+            Logger.d("GPS권한 요청 거부됨.")
+            getLocationUsagePermission()
+        case .authorizedAlways, .authorizedWhenInUse:
+            Logger.d("GPS권한 설정됨.")
+            //중요!
+            self.locationManager.startUpdatingLocation()
+        default:
+            Logger.d("GPS: Default")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // the most recent location update is at the end of the array.
+        let location: CLLocation = locations[locations.count - 1]
+        let longtitude: CLLocationDegrees = location.coordinate.longitude
+        let latitude: CLLocationDegrees = location.coordinate.latitude
+        Logger.i("\(longtitude), \(latitude)")
+    }
+}
