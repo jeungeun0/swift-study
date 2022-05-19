64bef673a630414ca706efa899a8776589768c16
JeungEun <anjeundeun@gmail.com>
2022-05-19 09:52:03 +0900

    Initial Commit

---
+++ PracticeAndStudyUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeAndStudyUI
+//
+//  Created by app on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
