4ddb1545ea63c855027d1f313cea655f87c1860a
JeungEun <anjeundeun@gmail.com>
2022-05-19 13:56:28 +0900

    first commit

---
+++ PracticeAndStudyUI/PageVC/CategoryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCollectionViewCell.swift
+//  PracticeAndStudyUI
+//
+//  Created by app on 2022/05/19.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PracticeAndStudyUI/PageVC/PageVCInContainerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageVCInContainerViewController.swift
+//  PracticeAndStudyUI
+//
+//  Created by app on 2022/05/19.
+//
+
+import UIKit
+
+class PageVCInContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
