bbb7fd34b6174bbc1d96a58002c2ca896ebaf156
JeungEun <deeprootedtree0@gmail.com>
2022-06-13 15:13:19 +0900

    ### 사용자 정보보기 화면 수정 1. 기본 이미지 설정 코드 추가 2. 사용자 정보가 하나도 없으면 팝업을 보여주고 이전 화면으로 이동하는 코드 추가 3. 코드 정리

--- login/StudyLogin/UserInfomationViewController.swift
+++ login/StudyLogin/UserInfomationViewController.swift
@@ -11,7 +11,11 @@ class UserInfomationViewController: UIViewController {
     
     //MARK: - Properties
     
-    @IBOutlet weak var ivThumbnail: UIImageView!
+    @IBOutlet weak var ivThumbnail: UIImageView! {
+        didSet {
+            ivThumbnail.image = UIImage(named: "no_image")
+        }
+    }
     @IBOutlet weak var stackViewOfInfomationCategorys: UIStackView!
     @IBOutlet weak var stackViewOfInfomations: UIStackView!
     
@@ -21,6 +25,24 @@ class UserInfomationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //데이터가 없으면 뒤로가기
+        if self.infomationData.count < 0 {
+            let alert = UIAlertController(title: "", message: "유저 정보가 없습니다.", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .default) { _ in
+                DispatchQueue.main.async {
+                    if let navigationController = self.navigationController {
+                        navigationController.popViewController(animated: true)
+                    } else {
+                        self.dismiss(animated: true)
+                    }
+                }
+            }
+            
+            alert.addAction(ok)
+            self.present(alert, animated: true)
+            return
+        }
+        
         //스택뷰 초기화
         stackViewOfInfomationCategorys.arrangedSubviews.forEach { view in
             stackViewOfInfomationCategorys.removeArrangedSubview(view)
@@ -31,27 +53,29 @@ class UserInfomationViewController: UIViewController {
             view.removeFromSuperview()
         }
         
-        //카테고리에 따른 라벨을 스택뷰에 추가
-        func createInformationLabel() {
-            guard self.infomationData.count > 0 else { return }
+        createInformationLabel()
+    }
+    
+    
+    /// 카테고리에 따른 라벨을 스택뷰에 추가
+    func createInformationLabel() {
+        guard self.infomationData.count > 0 else { return }
+        
+        self.infomationData.enumerated().forEach { index, item in
             
-            self.infomationData.enumerated().forEach { index, item in
-                
-                let lblCategory = UILabel()
-                let lblInfomation = UILabel()
-                
-                if let text = item.1 as? String {
-                    lblCategory.text = item.0.rawValue
-                    lblInfomation.text = text
-                    stackViewOfInfomationCategorys.addArrangedSubview(lblCategory)
-                    stackViewOfInfomations.addArrangedSubview(lblInfomation)
-                } else if let image = item.1 as? UIImage {
-                    self.ivThumbnail.image = image
-                    self.ivThumbnail.contentMode = .scaleAspectFill
-                }
-                
+            let lblCategory = UILabel()
+            let lblInfomation = UILabel()
+            
+            if let text = item.1 as? String {
+                lblCategory.text = item.0.rawValue
+                lblInfomation.text = text
+                stackViewOfInfomationCategorys.addArrangedSubview(lblCategory)
+                stackViewOfInfomations.addArrangedSubview(lblInfomation)
+            } else if let image = item.1 as? UIImage {
+                self.ivThumbnail.image = image
+                self.ivThumbnail.contentMode = .scaleAspectFill
             }
+            
         }
     }
-
 }
